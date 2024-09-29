eb7c1517072ac42c8b3b4bb80a2896856b6cc452
Aaron <afriedman111@gmail.com>
2024-09-29 15:44:08 -0400

    about bannder and updated about text

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HomeCallToActionComponent } from '../home-content/home-call-to-action/home-call-to-action.component';
 import { HomeNewsletterComponent } from '../home-content/home-newsletter/home-newsletter.component';
+import { AboutBannerComponent } from './about-banner/about-banner.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [HomeCallToActionComponent, HomeNewsletterComponent],
+  imports: [HomeCallToActionComponent, HomeNewsletterComponent, AboutBannerComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
